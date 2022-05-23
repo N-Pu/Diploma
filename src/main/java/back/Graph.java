dc71f30f045c0770e7394aa2e6207846129bffcd
Harry.G <garry.garyra@mail.ru>
2022-05-23 21:49:04 +0300

    Added Vectors in HashMap

--- src/main/java/back/Graph.java
+++ src/main/java/back/Graph.java
@@ -1,34 +1,41 @@
 package back;
 
 import java.util.Map;
-import java.util.Random;
 import java.util.Vector;
 
 public class Graph {
 
     Map<String, Boolean> graph; // = new HashMap<>();
-    Vector<String> nodes = new Vector<>();
+    Vector<String> nodesI = new Vector<>();
+    Vector<String> nodesC = new Vector<>();
 
-    public Graph(Map<String, Boolean> graph) {
-        this.graph = graph;
-    }
+
 
 
     public void CreateGraph(int IndependentNodes, int Clique) {
 
-        Random random = new Random();
-        random.ints(13);
-
-        for (int i = 0; i < IndependentNodes; i++) {
-            graph.put("C" + i + 1, new Vector<String>().add("I" + i + 1));
+        for (int k = 0; k < IndependentNodes; k++) {
+            nodesI.addElement("I" + k);
+            System.out.println(nodesI);
         }
 
-        for (int i = 0; i < Clique; i++) {
-            graph.put("I" + i + 1, new Vector<String>().add("C" + i + 1));
+        for (int j = 0; j < Clique; j++) {
+//            System.out.println(nodesC.add("C" + j));
+            nodesC.addElement("C" + j);
+            System.out.println(nodesC);
         }
+
+
+//        for (int i = 0; i < IndependentNodes; i++) {
+//            graph.put("C" + i + 1, new Vector<String>().add("I" + i));
+//        }
+//
+//        for (int i = 0; i < Clique; i++) {
+//            graph.put("I" + i + 1, new Vector<String>().add("C" + i));
 
 //        graph.put("C1",new Vector<String>().add("I1"));
 
+
+        }
     }
 
-}
--- src/main/java/back/MainClass.java
+++ src/main/java/back/MainClass.java
@@ -6,14 +6,18 @@ import java.util.Vector;
 
 public class MainClass {
     public static void main(String[] args) {
-        Math math = new Integer();
-        math
+
 
 //        Vector<String> nodes = new Vector<>();
 
-        Random random = new Random();
-        random.ints(13);
-        System.out.println(random.);
+//        Random random = new Random();
+//        random.ints(13);
+//        System.out.println(random);
+
+        Graph graph = new Graph();
+        graph.CreateGraph(10,71);
+//        System.out.println(graph);
+
 
     }
 }
